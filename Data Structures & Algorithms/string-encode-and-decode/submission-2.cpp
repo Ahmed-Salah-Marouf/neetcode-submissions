a1db1b850159b4b4dd9a38fe46217d36f91bc212
Ahmed Salah <elshitehi1@gmail.com>
2026-07-14 22:17:57 +0300

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+        for (int i = 0; i < strs.size(); i++) {
+            encoded += to_string(strs[i].size());
+            encoded += '-';
+            encoded += strs[i];
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int m = s.size();
+        int i = 0;
+        cout << s << '\n';
+        while (i < m) {
+            string num;
+            while(s[i] != '-') {
+                num+=s[i++];
+                cout << num << '\n';
+            }
+            i++;
+            int n = stoi(num);
+            string temp;
+            while(n--) {
+                temp.push_back(s[i++]);
+            }
+            decoded.push_back(temp);
+        }
+        return decoded;
+    }
+};
