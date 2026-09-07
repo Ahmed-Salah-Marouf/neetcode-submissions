a3b989d4b2605a5506e30141a54849886c561843
Ahmed Salah <elshitehi1@gmail.com>
2026-09-07 22:04:27 +0300

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* merged = nullptr;
+        ListNode* answer = nullptr;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                if (merged == nullptr) {
+                    merged = list1;
+                    list1 = list1->next;
+                    answer = merged;
+                } else {
+                    merged->next = list1;
+                    list1 = list1->next;
+                    if (merged->next) merged = merged->next;
+                }
+            } else {
+                if (merged == nullptr) {
+                    merged = list2;
+                    list2 = list2->next;
+                    answer = merged;
+                } else {
+                    merged->next = list2;
+                    list2 = list2->next;
+                    if (merged->next) merged = merged->next;
+                }
+            }
+        }
+        while (list1) {
+            if (merged == nullptr) {
+                merged = list1;
+                list1 = list1->next;
+                answer = merged;
+            } else {
+                merged->next = list1;
+                list1 = list1->next;
+                merged = merged->next;
+            }
+        }
+
+        while (list2) {
+            if (merged == nullptr) {
+                merged = list2;
+                list2 = list2->next;
+                answer = merged;
+            } else {
+                merged->next = list2;
+                list2 = list2->next;
+                merged = merged->next;
+            }
+        }
+        return answer;
+    }
+};
